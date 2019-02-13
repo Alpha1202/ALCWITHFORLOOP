040e43cc7cb816c322ac550fb468cdd3bbcf4816
Nzubechukwu <nzubennamani@gmail.com>
2019-02-13 20:05:20 +0100

    modified API folder

---
+++ API/index.js
@@ -0,0 +1,15 @@
+const express = require ('express');
+
+
+
+
+const app = express();
+const PORT = 4001;
+
+app.get('/', function(){
+    return res.send
+})
+
+app.listen(PORT, function(){
+    console.log('server is running on port' + PORT);
+})

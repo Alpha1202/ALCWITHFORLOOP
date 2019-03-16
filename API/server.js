47af59af8ebfe01a50e8fae2fc04ddf5e8f95fa8
Nzubechukwu <nzubennamani@gmail.com>
2019-03-16 17:23:58 +0100

    integrating swagger

--- API/server.js
+++ API/server.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser';
 import routes from './routes';
 import db from '../models';
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 app.use(bodyParser.json());
